e5ac89a72a9168ebd6c2f2306aae094a2e8f73a9
filypsdias <filipedias1708@gmail.com>
2022-04-07 21:59:57 -0300

    :sparkles: Creating initial structure of the code

---
+++ src/main/java/br/com/sicredi/cpfvalidator/CpfValidatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.cpfvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredi/cpfvalidator/CpfValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.cpfvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
